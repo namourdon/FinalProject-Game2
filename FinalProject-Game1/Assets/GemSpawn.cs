258225ce920a5605d12441bd567cc050c3d7c7ee
namourdon <nash.amourdon@gmail.com>
2015-12-03 14:28:24 -0500

    added script

---
+++ FinalProject-Game1/Assets/GemSpawn.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GemSpawn : MonoBehaviour {
+
+    public float speed;
+    // Use this for initialization
+    void Start()
+    {
+        this.Reset();
+    }
+
+    // Update only once per frame
+    void Update()
+    {
+        Vector2 currentPositon = new Vector2(0.0f, 0.0f);
+        currentPositon = gameObject.GetComponent<Transform>().position;
+        currentPositon.x -= speed;
+        gameObject.GetComponent<Transform>().position = currentPositon;
+
+        if (currentPositon.x <= -990)
+        {
+            this.Reset();
+        }
+
+    }
+    private void Reset()
+    {
+        Vector2 resetBackground = new Vector2(990f, Random.Range(-490f, 490f));
+        gameObject.GetComponent<Transform>().position = resetBackground;
+    }
+}
