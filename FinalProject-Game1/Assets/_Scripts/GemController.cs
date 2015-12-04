49eb3708e189dd55eef28be6fb8013ce46d7c4a4
namourdon <nash.amourdon@gmail.com>
2015-12-04 13:08:53 -0500

    Added Scripts and enemies

---
+++ FinalProject-Game1/Assets/_Scripts/GemController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GemController : MonoBehaviour {
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnCollisionEnter2D(Collision2D otherCollider)
+    {
+        if (otherCollider.gameObject.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ FinalProject-Game1/Assets/_Scripts/GemSpawn.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GemSpawn : MonoBehaviour {
+    public Transform[] coinSpawn;
+    public GameObject coin;
+    // Use this for initialization
+    void Start()
+    {
+        Spawn();
+    }
+
+    // Update is called once per frame
+    void Spawn()
+    {
+        //pour faire les coins devenir beaucoup plus
+        for (int i = 0; i < coinSpawn.Length; i++)
+        {
+            int coinFlip = Random.Range(0, 11);
+            if (coinFlip > 0)
+            {
+                Instantiate(coin, coinSpawn[i].position, Quaternion.identity);
+            }
+        }
+    }
+
+    
+
+    
+   
+}
