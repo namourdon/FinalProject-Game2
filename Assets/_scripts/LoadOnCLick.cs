ec536d56cb3d85456e25803fa33e76a8bcb748d5
Sukhpreet Sekhon <ersukhpreetsekhon03@gmail.com>
2015-12-18 11:47:28 -0500

    added load level script to all buttons

---
+++ Assets/_scripts/LoadOnCLick.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadOnCLick : MonoBehaviour {
+
+    public void LoadScene( int level)
+    {
+        Application.LoadLevel(level);
+            
+            
+            
+            
+            
+     
+    }
+}
